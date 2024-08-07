9b862a20f35f30ecbcf83bb7f1266789768da01e
P81000 <p.masteguin@unifesp.br>
2024-08-07 20:06:28 -0300

    Testing

--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,32 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView,
+        meta: {
+            title: "Home - YaYa Score"
+        }
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path: "/login",
+        name: "login",
+        componet: () => import(/* webpackChunkName: "login" */ "../views/LoginView.vue")
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login">
+        <div class="tittle">
+            teste
+        </div>
+        <form @submit.prevent="login">
+            <div class="input-group">
+                <label for="publicKey">Chave Pública</label>
+                <input type="text" v-model="publicKey" required />
+            </div>
+            <div class="input-group">
+                <label for="privateKey">Chave Privada</label>
+                <input type="password" v-model="privateKey" required />
+            </div>
+            <div class="input-group">
+                <label for="network">Network</label>
+                <div class="custom-select">
+                    <select v-model="network">
+                        <option value="mainnet">Mainnet</option>
+                        <option value="ropsten">Ropsten</option>
+                        <option value="ganache">Ganache</option>
+                    </select>
+                </div>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
